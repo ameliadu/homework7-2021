7fe4c6e017d49093388fcd5402e35b7386e7e9b9
ameliadu <71349092+ameliadu@users.noreply.github.com>
2021-04-01 19:20:14 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -23,13 +23,27 @@ document.querySelector("#pause").addEventListener("click",function(){
 document.querySelector("#slower").addEventListener("click", function(){
 	console.log("Slow Video");
 	video.playbackRate *= 0.95;
-	console.log(video.playbackRate)
+	console.log(video.playbackRate);
 })
 
 document.querySelector("#faster").addEventListener("click", function(){
 	console.log("Fast Video");
-	video.playbackRate %= .95;
-	console.log(video.playbackRate)
+	video.playbackRate *= 1.05;
+	console.log(video.playbackRate);
+})
+
+document.querySelector("#skip").addEventListener("click", function(){
+	console.log("skip ahead");
+	video.curentTime(video.currentTime()+10);
+	console.log(video.currentTime)
+})
+
+document.querySelector("#slider").addEventListener("click", function(){
+	console.log(this.value);
+	var slider = this.value;
+	video.volume = slider / 100;
+	document.getElementById("volume").innerHTML = video.volume * 100 + "%"
+	
 })
 
 document.querySelector("#vintage").addEventListener("click",function(){
@@ -44,11 +58,13 @@ document.querySelector("#orig").addEventListener("click",function(){
 
 document.querySelector("#mute").addEventListener("click",function(){
 	console.log("Muting");
-	video.muted = true;
-	//if video.muted == true
-	//video.muted = false
-	//else
-	//video.muted = true
+	if (video.muted == true){
+		video.muted = false;
+	}
+	else{
+		video.muted = true
+	}
+
 });
 
 
